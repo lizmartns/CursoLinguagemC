06bac36f2db585de323510644a4989df3156d30f
Liz Martins <martins.flordeliz@gmail.com>
2024-10-15 08:19:07 -0300

    exercícios

---
+++ diretivas/compilando.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int qtd, soma=0;
+    printf("Quantos números você quer somar?");
+    scanf("%d", &qtd);
+
+    for(int i=0; i<qtd; i++){
+        soma = soma + i *2 +3;
+    }
+    printf("A soma é %d", soma);
+
+    return 0;
+
+}
